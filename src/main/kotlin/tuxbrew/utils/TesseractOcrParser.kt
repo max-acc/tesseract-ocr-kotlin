bc886e05a160dea4e47e7c7ccbe6eadfd117357e
alien <max.wenk@protonmail.com>
2026-01-16 11:17:04 +0100

    add: image ocr

--- src/main/kotlin/tuxbrew/utils/TesseractOcrParser.kt
+++ src/main/kotlin/tuxbrew/utils/TesseractOcrParser.kt
@@ -1,10 +1,22 @@
 package alien42.tuxbrew.utils
 
 import net.sourceforge.tess4j.Tesseract
+import java.io.InputStream
+import javax.imageio.ImageIO
 
 /**
  * This class parses data using a tesseract OCR parser.
  */
 class TesseractOcrParser {
-    private val tesseract = Tesseract()
+    private val tesseract = Tesseract().apply {
+        setDatapath("tessdata")
+        setLanguage("eng")
+    }
+
+    fun generateTextFromImage(imageStream : InputStream) : String {
+        val image = ImageIO.read(imageStream)
+            ?: throw IllegalStateException("Invalid image input stream") // TODO handle this error
+
+        return tesseract.doOCR(image)
+    }
 }
